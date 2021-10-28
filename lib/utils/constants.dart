e3b9df380079ef4d28006e9c2daf158f9a7ebbaa
RakulAgn <rakul0agn@gmail.com>
2021-10-28 21:55:48 +0530

    MongoDb Flutter

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 const mongoUrl =
     "mongodb+srv://$userId:$userPassword@cluster0.cfvup.mongodb.net/$userData?retryWrites=true&w=majority";
 const usercollection = "Text";
-const userId = "MonkAno";
-const userPassword = "MonkAno";
+const userId = "Yours";
+const userPassword = "Yours";
 const userData = "FruitsData";
